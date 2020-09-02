65caf1ef8aba1b42ea1c7afd2db9c2391f0ead90
Kanav Khera <knvkhera30@gmail.com>
2020-09-03 02:34:58 +0530

    routing done

--- server.js
+++ server.js
@@ -1,4 +1,6 @@
 const express = require("express");
+const { response } = require("express");
+const nodemon = require("nodemon");
 
 const app = express();
 
@@ -7,6 +9,13 @@ reponse.send("hello world");
 }
 );
 
+app.get("/contact", function(req,res){
+res.send("contact me at: knvkhera30@gmail.com");
+});
+
+app.get("/about", function(req,res){
+    res.send("I am pursuing my bachelors from cgc landra");
+});
 app.listen(3000, function(){
  console.log('server has started');
 });
